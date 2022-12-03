c19168bf13e9740daa2546add7fc66efe4c4c1ce
Patton97 <atpatton97@gmail.com>
2022-12-03 23:09:23 +0000

    Day 1

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AdventOfCode2022
+{
+    class Day1 : IDay
+    {
+        public void Solve()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Inputs\Day1Input.txt");
+            string[] lines = File.ReadAllLines(path);
+            int currentCalorieCount = 0;
+            int highestCalorieCount = 0;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    UpdateHighestIfHigher(ref currentCalorieCount, ref highestCalorieCount);
+                    continue;
+                }
+
+                currentCalorieCount += int.Parse(line);
+            }
+
+            Console.WriteLine(highestCalorieCount);
+            Console.Read();
+        }
+
+        void UpdateHighestIfHigher(ref int calorieCount, ref int highestCalorieCount)
+        {
+            if (highestCalorieCount < calorieCount)
+            {
+                highestCalorieCount = calorieCount;
+            }
+            calorieCount = 0;
+        }
+    }
+}
