6f287250155e4850e44ea629e6e66355e948b1a6
Patton97 <atpatton97@gmail.com>
2022-12-04 04:06:58 +0000

    Day 3 Part 2

--- AdventOfCode2022/Days/Day3/Day3.cs
+++ AdventOfCode2022/Days/Day3/Day3.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode2022.Days.Day3;
 
@@ -12,17 +10,14 @@ internal class Day3 : Day, IDay3
 
     public override void SolvePart1()
     {
-        string[] lines = this.ReadLines();
         int sum = 0;
-        foreach (string line in lines)
+        foreach (string line in this.ReadLines())
         {
             IEnumerable<char> itemsInCompartment1 = line.Take(line.Length / 2);
             IEnumerable<char> itemsInCompartment2 = line.Skip(line.Length / 2);
             IEnumerable<char> itemsInBothCompartments = itemsInCompartment1
                 .Intersect(itemsInCompartment2);
-            sum += itemsInBothCompartments
-                .Select(item => Array.IndexOf(this.priority, item))
-                .Sum();
+            sum += this.GetSumOfPriorities(itemsInBothCompartments);
         }
 
         Console.WriteLine($"Sum: {sum}");
@@ -30,6 +25,31 @@ internal class Day3 : Day, IDay3
 
     public override void SolvePart2()
     {
+        int sum = 0;
+        int numElvesInGroup = 3;
+        string[] elfInventories = this.ReadLines();
+        for (int i = 0; i < elfInventories.Length; i += numElvesInGroup)
+        {
+            IEnumerable<char> intersectOfItems = elfInventories[i];
+            for (int j = 1; j < numElvesInGroup; ++j)
+            {
+                intersectOfItems = intersectOfItems.Intersect(elfInventories[i + j]);
+            }
+            char badge = intersectOfItems.Single();
 
+            sum += this.GetPriority(badge);
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+
+    int GetSumOfPriorities(IEnumerable<char> items)
+    {
+        return items.Select(this.GetPriority).Sum();
+    }
+
+    int GetPriority(char item)
+    {
+        return Array.IndexOf(this.priority, item);
     }
 }
