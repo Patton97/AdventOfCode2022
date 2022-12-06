92c5a6816a4ec53a99d8fff1a78cc14dc3660328
Patton97 <atpatton97@gmail.com>
2022-12-06 21:27:57 +0000

    Quick refactor of Day 6 for the sake of it

--- AdventOfCode2022/Days/Day6/Day6.cs
+++ AdventOfCode2022/Days/Day6/Day6.cs
@@ -9,8 +9,19 @@ internal class Day6 : Day, IDay6
     /// <inheritdoc cref="IDay6.SolvePart1"/>
     public override void SolvePart1()
     {
+        this.FindMarker(4);
+    }
+
+    /// <inheritdoc cref="IDay6.SolvePart2"/>
+    public override void SolvePart2()
+    {
+        this.FindMarker(14);
+    }
+
+    void FindMarker(int markerLength)
+    {
         string dataStream = this.ReadLines()[0];
-        var potentialMarker = new Queue<char>(dataStream.Take(4));
+        var potentialMarker = new Queue<char>(dataStream.Take(markerLength));
         int i = potentialMarker.Count;
         for (; i < dataStream.Length - 1; ++i)
         {
@@ -27,23 +38,5 @@ internal class Day6 : Day, IDay6
     bool IsValidMarker(IEnumerable<char> potentialMarker)
     {
         return potentialMarker.ToHashSet().Count == potentialMarker.Count();
-    }
-
-    /// <inheritdoc cref="IDay6.SolvePart2"/>
-    public override void SolvePart2()
-    {
-        string dataStream = this.ReadLines()[0];
-        var potentialMarker = new Queue<char>(dataStream.Take(14));
-        int i = potentialMarker.Count;
-        for (; i < dataStream.Length - 1; ++i)
-        {
-            if (this.IsValidMarker(potentialMarker))
-            {
-                break;
-            }
-            potentialMarker.Dequeue();
-            potentialMarker.Enqueue(dataStream[i]);
-        }
-        Console.WriteLine($"First marker '{string.Concat(potentialMarker)}' ends at index {i}");
     }
 }
